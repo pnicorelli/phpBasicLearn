98f68109b4b89b4443e2fccc2069859e9863fa1d
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2015-07-01 17:48:22 +0200

    comments improvements

--- login.php
+++ login.php
@@ -1,8 +1,19 @@
 <?php
+//show all errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+//fix some LAMP distributions error
+session_start();
+
+//dump every vars in the SESSION
 echo "SESSION: ";
+echo "<code>";
 var_dump($_SESSION);
+echo "</code>";
 
-
+//dump every vars in REQUEST (GET/POST)
 echo "<br><br>REQUEST: ";
+echo "<code>";
 var_dump($_REQUEST);
+echo "</code>";
